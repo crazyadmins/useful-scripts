8e39311b27752fcf571887e2d34d6b3c1931a806
Kuldeep Kulkarni <kuldeepkulkarni09@gmail.com>
2016-07-10 19:56:18 -0700

    Added scripts for automated ambari/hdp installation

---
+++ hdp-automated-setup/generate_json.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+########
+# Author: Kuldeep Kulkarni 
+# Description: This script does the Magic of automating HDP install using Ambari Blueprints
+set -x
+
+#Globals
+LOC=`pwd`
+PROPS=$1
+#Source props
+source $LOC/$PROPS 2>/dev/null
+STACK_VERSION=`echo $CLUSTER_VERSION|cut -c1-3`
+AMBARI_HOST=$2
+NUMBER_OF_HOSTS=`grep HOST $LOC/$PROPS|grep -v SERVICES|wc -l`
+LAST_HOST=`grep HOST $LOC/$PROPS|grep -v SERVICES|head -n $NUMBER_OF_HOSTS|tail -1|cut -d'=' -f2`
+grep HOST $LOC/$PROPS|grep -v SERVICES|grep -v $LAST_HOST|cut -d'=' -f2 > $LOC/list
+OS_VERSION=`echo $OS|rev|cut -c1|rev`
+
+#Generate hostmap function#
+
+hostmap()
+{
+#Start of function
+
+echo "{
+  \"blueprint\" : \"$CLUSTERNAME\",
+  \"default_password\" : \"$DEFAULT_PASSWORD\",
+  \"host_groups\" :["
+
+for HOST in `cat list`
+do
+   echo "{
+      \"name\" : \"$HOST\",
+      \"hosts\" : [
+        {
+          \"fqdn\" : \"$HOST.$DOMAIN_NAME\"
+        }
+      ]
+    },"
+done
+
+echo "{
+      \"name\" : \"$LAST_HOST\",
+      \"hosts\" : [
+        {
+          \"fqdn\" : \"$LAST_HOST.$DOMAIN_NAME\"
+        }
+      ]
+    }
+  ]
+}"
+
+#End of function
+}
+
+clustermap()
+{
+#Start of function
+LAST_HST_NAME=`grep 'HOST[0-9]*' $LOC/$PROPS|grep -v SERVICES|tail -1|cut -d'=' -f1`
+
+echo "{
+  \"configurations\" : [ ],
+  \"host_groups\" : ["
+
+for HOST in `grep -w 'HOST[0-9]*' $LOC/$PROPS|tr '\n' ' '`
+do
+   HST_NAME_VAR=`echo $HOST|cut -d'=' -f1`
+   echo "{
+      \"name\" : \"`grep $HST_NAME_VAR $PROPS |head -1|cut -d'=' -f2|cut -d'.' -f1`\",
+      \"components\" : ["
+		LAST_SVC=`grep $HST_NAME_VAR"_SERVICES" $LOC/$PROPS|cut -d'=' -f2|tr ',' ' '|rev|cut -d' ' -f1|rev|cut -d'"' -f1`
+		for SVC in `grep $HST_NAME_VAR"_SERVICES" $LOC/$PROPS|cut -d'=' -f2|tr ',' ' '|cut -d'"' -f2|cut -d'"' -f1`
+		do
+        		echo "{
+			\"name\" : \"$SVC\""
+			if [ "$SVC" == "$LAST_SVC" ]
+			then
+				echo "}
+				],
+      			        \"cardinality\" : "1""
+				if [ "$HST_NAME_VAR" == "$LAST_HST_NAME" ]
+				then
+    	               		    	echo "}"
+				else
+					echo "},"
+				fi
+			else
+       	 				echo "},"
+			fi
+		done
+done
+
+echo "  ],
+  \"Blueprints\" : {
+    \"blueprint_name\" : \"$CLUSTERNAME\",
+    \"stack_name\" : \"HDP\",
+    \"stack_version\" : \"$STACK_VERSION\"
+  }
+}"
+
+
+#End of function
+}
+
+
+repobuilder()
+{
+#Start of function
+BASE_URL=`grep $CLUSTER_VERSION $LOC/$PROPS|grep BASE|cut -d'=' -f2|sed -e 's/^"//'  -e 's/"$//'|grep $OS|sort -n|tail -1`
+
+echo "{
+\"Repositories\" : {
+   \"base_url\" : \"$BASE_URL\",
+   \"verify_base_url\" : true
+}
+}" > $LOC/repo.json
+
+BASE_URL_UTILS=`grep UTILS $LOC/$PROPS|cut -d'=' -f2|sed -e 's/^"//'  -e 's/"$//'|grep $OS|sort -n|tail -1`
+export BASE_URL_UTILS;
+
+echo "{
+\"Repositories\" : {
+   \"base_url\" : \"$BASE_URL_UTILS\",
+   \"verify_base_url\" : true
+}
+}" > $LOC/repo-utils.json
+
+#End of function
+}
+
+timestamp()
+{
+#Function to print timestamp
+
+echo "`date +%Y-%m-%d-%H:%M:%S`"
+}
+
+installhdp()
+{
+#Install hdp using Ambari Blueprints
+
+HDP_UTILS_VERSION=`echo $BASE_URL_UTILS| awk -F'/' '{print $6}'`
+
+curl -H "X-Requested-By: ambari" -X POST -u admin:admin http://$AMBARI_HOST:8080/api/v1/blueprints/$CLUSTERNAME -d @"$LOC"/cluster_config.json
+sleep 1
+curl -H "X-Requested-By: ambari" -X PUT -u admin:admin http://$AMBARI_HOST:8080/api/v1/stacks/HDP/versions/$STACK_VERSION/operating_systems/redhat"$OS_VERSION"/repositories/HDP-$STACK_VERSION -d @$LOC/repo.json
+sleep 1
+curl -H "X-Requested-By: ambari" -X PUT -u admin:admin http://$AMBARI_HOST:8080/api/v1/stacks/HDP/versions/$STACK_VERSION/operating_systems/redhat"$OS_VERSION"/repositories/$HDP_UTILS_VERSION -d @$LOC/repo-utils.json
+sleep 1
+curl -H "X-Requested-By: ambari" -X POST -u admin:admin http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTERNAME -d @$LOC/hostmap.json
+
+}
+
+#################
+# Main function #
+################
+
+#Generate hostmap
+echo -e "`timestamp` Generating hostmap json.."
+hostmap > $LOC/hostmap.json
+echo "`timestamp` Saved $LOC/hostmap.json"
+
+#Generate cluster config json
+echo -e "`timestamp` Generating cluster configuration json"
+clustermap > $LOC/cluster_config.json
+echo "`timestamp` Saved $LOC/cluster_config.json"
+
+#Create internal repo json 
+repobuilder 
+echo -e "`timestamp` Generating internal repositories json..\n`timestamp` Saved $LOC/repo.json & $LOC/repo-utils.json"
+
+#Start hdp installation
+installhdp
---
+++ hdp-automated-setup/setup_cluster.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#Author - Kuldeep Kulkarni
+#Script will setup and configure ambari-server/ambari-agents and hdp cluster
+##########################################################
+
+if [ $# -ne 1 ]
+then
+        echo -e "Usage $0 /path-to/cluster.props\nExample: $0 templates/multi-node/cluster.props"
+        exit
+fi
+
+#Cleanup
+rm -rf ~/.ssh/known_hosts
+
+#Globals
+LOC=`pwd`
+CLUSTER_PROPERTIES=$1
+source $LOC/$CLUSTER_PROPERTIES 2>/dev/null
+AMBARI_SERVER=`grep -w HOST[0-9] $LOC/$CLUSTER_PROPERTIES|head -1|cut -d'=' -f2`.$DOMAIN_NAME
+AMBARI_AGENTS=`grep -w HOST[0-9] $LOC/$CLUSTER_PROPERTIES|cut -d'=' -f2` 2>/dev/null
+AMBARI_SERVER_IP=`grep -w $AMBARI_SERVER /etc/hosts|awk '{print $1}'`
+
+
+generate_ambari_repo()
+{
+#This will generate internal repo file for Ambari Setup
+echo "[Updates-ambari-$AMBARIVERSION]
+name=ambari-$AMBARIVERSION - Updates
+baseurl=http://172.26.64.249/ambari/$OS/Updates-ambari-$AMBARIVERSION/
+gpgcheck=0
+enabled=1
+priority=1" > /tmp/ambari-$AMBARIVERSION.repo
+}
+
+prepare_hosts_file()
+{
+	echo -e "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4\n::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" > /tmp/hosts
+	for host in `grep -w HOST[0-9] $LOC/$CLUSTER_PROPERTIES|cut -d'=' -f2`; do grep $host /etc/hosts >> /tmp/hosts;done
+}	
+
+bootstrap_hosts()
+{
+	
+        for host in `echo $AMBARI_AGENTS`
+        do
+                HOST=`echo $host`.$DOMAIN_NAME
+                scp -o "StrictHostKeyChecking no" /tmp/ambari-"$AMBARIVERSION".repo root@$HOST:/etc/yum.repos.d/
+                scp -o "StrictHostKeyChecking no" /tmp/hosts root@$HOST:/etc/hosts
+		ssh -o "StrictHostKeyChecking no" root@$HOST hostname "$HOST"
+		ssh -o "StrictHostKeyChecking no" root@$HOST service iptables stop
+		ssh -o "StrictHostKeyChecking no" root@$HOST chkconfig iptables off
+		if [ "$OS" == "centos7" ]
+		then
+			ssh -o "StrictHostKeyChecking no" root@$HOST hostnamectl set-hostname "$HOST" --static
+		fi
+        done
+}
+
+setup_ambari_server()
+{
+	ssh -o "StrictHostKeyChecking no" root@$AMBARI_SERVER yum -y install ambari-server
+	ssh -o "StrictHostKeyChecking no" root@$AMBARI_SERVER ambari-server setup -s
+	ssh -o "StrictHostKeyChecking no" root@$AMBARI_SERVER ambari-server start
+}
+
+setup_ambari_agent()
+{
+	for host in `echo $AMBARI_AGENTS`
+	do
+		AMBARI_AGENT=`echo $host`.$DOMAIN_NAME
+		ssh -o "StrictHostKeyChecking no" root@$AMBARI_AGENT yum -y install ambari-agent
+		ssh -o "StrictHostKeyChecking no" root@$AMBARI_AGENT ambari-agent reset $AMBARI_SERVER 
+		ssh -o "StrictHostKeyChecking no" root@$AMBARI_AGENT service ambari-agent start 
+	done
+}
+
+setup_hdp()
+{
+	$LOC/generate_json.sh $CLUSTER_PROPERTIES $AMBARI_SERVER_IP
+}
+
+
+#Main starts here
+
+
+generate_ambari_repo
+prepare_hosts_file
+bootstrap_hosts
+setup_ambari_server
+setup_ambari_agent
+sleep 5
+setup_hdp
