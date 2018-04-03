535da9a4643a50272175ed514bba4a51173dcb4d
Kuldeep KulKarni <kkulkarni@HW13054.local>
2018-04-02 17:07:10 -0700

    Modified payload

--- ambari/setup_kerberos.sh
+++ ambari/setup_kerberos.sh
@@ -50,7 +50,7 @@ create_payload()
         \"tag\": \"version1\",
         \"properties\": {
           \"domains\":\"\",
-          \"manage_krb5_conf\": \"false\",
+          \"manage_krb5_conf\": \"true\",
           \"conf_dir\":\"/etc\",
 	  \"content\" : \"[libdefaults]\n  renew_lifetime = 7d\n  forwardable= true\n  default_realm = {{realm|upper()}}\n  ticket_lifetime = 24h\n  dns_lookup_realm = false\n  dns_lookup_kdc = false\n  #default_tgs_enctypes = {{encryption_types}}\n  #default_tkt_enctypes ={{encryption_types}}\n\n{% if domains %}\n[domain_realm]\n{% for domain in domains.split(',') %}\n  {{domain}} = {{realm|upper()}}\n{% endfor %}\n{%endif %}\n\n[logging]\n  default = FILE:/var/log/krb5kdc.log\nadmin_server = FILE:/var/log/kadmind.log\n  kdc = FILE:/var/log/krb5kdc.log\n\n[realms]\n  {{realm}} = {\n    admin_server = {{admin_server_host|default(kdc_host, True)}}\n    kdc = {{kdc_host}}\n }\n\n{# Append additional realm declarations below #}\n\"
         }
@@ -68,7 +68,7 @@ create_payload()
           \"install_packages\": \"true\",
           \"encryption_types\": \"aes des3-cbc-sha1 rc4 des-cbc-md5\",
           \"realm\" : \"$REALM\",
-          \"kdc_host\" : \"$KDC_HOST\",
+          \"kdc_hosts\" : \"$KDC_HOST\",
           \"admin_server_host\" : \"$KDC_HOST\",
           \"executable_search_paths\" : \"/usr/bin, /usr/kerberos/bin, /usr/sbin, /usr/lib/mit/bin, /usr/lib/mit/sbin\",
           \"password_length\": \"20\",
